546fe8e9690ede390105b66657936d76d83b51ca
Md Sohail <sohil931376@gmail.com>
2025-06-12 16:19:37 +0530

    fix buy home page

--- src/Components/Contact.jsx
+++ src/Components/Contact.jsx
@@ -50,7 +50,7 @@ const Contact = () => {
         })
         setFormConfirmMsg('Thanks! Your message has been sent.');
         setFormSubmited(true);
-        setTimeOut(()=>{
+        setTimeout(()=>{
         setFormSubmited("")
       },4000)
         return 
@@ -59,7 +59,7 @@ const Contact = () => {
       // if no server error and response status more than 300
       setFormConfirmMsg('Sorry, Your message has been not sent!');
       setFormSubmited(false);
-      setTimeOut(()=>{
+      setTimeout(()=>{
         setFormSubmited("")
       },4000)
       return
@@ -67,7 +67,7 @@ const Contact = () => {
     //  Run if have server error or server down 
     setFormConfirmMsg('Sorry, Server was Down, try later!');
     setFormSubmited(false);
-    setTimeOut(()=>{
+    setTimeout(()=>{
       setFormSubmited("")
     },4000)
   }
@@ -77,15 +77,15 @@ const Contact = () => {
       <div className="flex-1 md:max-w-[400px] lg:min-w-[300px] pb-4 pt-0 md:py-4 px-0 text-center flex justify-center">
         <img loading="lazy" src={contactImage} alt="Conatct Us" className="w-full max-w-md mx-auto rounded-xl mix-blend-multiply" height="100%" width="100%" />
       </div>
+      <form  onSubmit={formSubmit} className="flex-1 w-full max-w-3xl grid gap-2 px-0 md:px-4 relative bg-transparent">
       {
         // Confirmation Message 
       }
       <div id="confirmation" className={`${formSubmited === '' && 'hidden'} ${formSubmited ? 'text-green-600' : 'text-red-600'} font-medium mb-6`}>{formConfirmMsg}</div>
-      <form  onSubmit={formSubmit} className="flex-1 w-full max-w-3xl grid gap-2 px-0 md:px-4 relative bg-transparent">
         <h3 className="text-3xl sm:text-3xl md:text-4xl text-center font-bold text-slate-700 pb-4 md:mb-1">Connect With Us</h3>
         <div className="absolute inset-0 z-[-1] blur-[100px] rounded-xl bg-[radial-gradient(circle_at_center,_rgba(255,120,110,0.4),_rgba(110,120,255,0.4))]"></div>
-        <input name='name' onChange={setInputValue} value={inputData.name} type="text" placeholder="Enter Name" required className="w-full p-3 rounded-md bg-slate-100 text-base shadow border border-transparent focus:outline-none focus:ring-2 focus:ring-indigo-400 transition" />
-        <input name='email' onChange={setInputValue} value={inputData.email} type="email" placeholder="Enter Email ID" required className="w-full p-3 rounded-md bg-slate-100 text-base shadow border border-transparent focus:outline-none focus:ring-2 focus:ring-indigo-400 transition" />
+        <input name='name' onChange={setInputValue} value={inputData.name} type="text" placeholder="Enter Name" required className="w-full p-3 rounded-md bg-slate-100 text-base shadow border border-transparent focus:outline-none focus:ring-2 focus:ring-indigo-400 transition" autocomplete="true"/>
+        <input name='email' onChange={setInputValue} value={inputData.email} type="email" placeholder="Enter Email ID" required className="w-full p-3 rounded-md bg-slate-100 text-base shadow border border-transparent focus:outline-none focus:ring-2 focus:ring-indigo-400 transition" autocomplete="true"/>
         <textarea name='message' onChange={setInputValue} value={inputData.message} placeholder="Message Here..." required className="w-full h-32 p-3 rounded-md bg-slate-100 text-base shadow border border-transparent resize-none focus:outline-none focus:ring-2 focus:ring-indigo-400 transition"></textarea>
         <button type="submit" className="mr-auto my-2" >
           <a className="inline-block px-6 py-3 bg-blue-600 text-white rounded-md transition duration-300 hover:bg-blue-800 ml-0 mr-auto">
